0edf2e148e35dfe6c032fa3de1e6a5e9aa82a7c3
Deepak Gupta <154800280+deepak-gupta-git@users.noreply.github.com>
2025-05-22 01:08:14 +0530

    Update CategorySelection.jsx

--- frontend/src/Components/CategorySelection.jsx
+++ frontend/src/Components/CategorySelection.jsx
@@ -12,7 +12,7 @@ const CategorySelection = (
       useEffect(() =>{
             const fetchBlogs = async() => {
                 try {
-                  let url = "http://localhost:8080/api/blogs";
+                  let url = "https://inkbot-ai.vercel.app/api/blogs";
     
                     // if(selectedCategory){
                     //   url += `?category=${selectedCategory}`;
