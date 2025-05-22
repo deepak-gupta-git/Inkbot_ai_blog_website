aa6d81ee30bf0fa29ebd285506ae78846e7e383f
Deepak Gupta <154800280+deepak-gupta-git@users.noreply.github.com>
2025-05-22 10:24:09 +0530

    Update CreateBlogs.jsx

--- frontend/Pages/CreateBlogs.jsx
+++ frontend/Pages/CreateBlogs.jsx
@@ -25,7 +25,7 @@ const CreateBlogs = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("https://inkbot-ai.vercel.app/api/blogs", {
+      const response = await fetch("https://inkbot-ai-blog-website.onrender.com/api/blogs", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
