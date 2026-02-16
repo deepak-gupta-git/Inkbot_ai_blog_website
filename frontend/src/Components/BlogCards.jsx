54bd8d339e7ac3614cff1a2724a2cb481572d240
Deepak Gupta <154800280+deepak-gupta-git@users.noreply.github.com>
2026-02-16 19:03:45 +0530

    Update BlogCards.jsx

--- frontend/src/Components/BlogCards.jsx
+++ frontend/src/Components/BlogCards.jsx
@@ -2,24 +2,42 @@ import React from 'react'
 import { FaUser } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 
-const BlogCards = ({blogs,selectedCategory}) => {
-    const filterBlogs = blogs.filter((blogs) => !selectedCategory || blogs.category === selectedCategory);
-    console.log(filterBlogs);
-    
+const BlogCards = ({ blogs = [], selectedCategory }) => {
+
+  if (!Array.isArray(blogs)) {
+    return <p>No blogs available</p>;
+  }
+
+  const filterBlogs = blogs.filter(
+    (blog) => !selectedCategory || blog.category === selectedCategory
+  );
+
   return (
     <div className='grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-8'>
-      {
-        filterBlogs.map((blogs) => <Link to={`/blogs/${blogs._id}`} key={blogs._id} className='p-5 shadow-lg rounded cursor-pointer'>
-        <div>
-            <img src={blogs.image} className='w-full' alt="" />
-        </div>
-        <h3 className='mt-4 mb-2 font-bold hover:text-blue-600 cursor-pointer'>{blogs.title}</h3>
-        <p className='mb- text-gray-600'>
-            <FaUser className='inline-flex mr-2 mb-1'/>
-            {blogs.author}</p>
-        <p className='text-sm text-gray-500'>Published: {blogs.published_date}</p>
-        </Link>)
-      }
+      {filterBlogs.map((blog) => (
+        <Link
+          to={`/blogs/${blog._id}`}
+          key={blog._id}
+          className='p-5 shadow-lg rounded cursor-pointer'
+        >
+          <div>
+            <img src={blog.image} className='w-full' alt="" />
+          </div>
+
+          <h3 className='mt-4 mb-2 font-bold hover:text-blue-600 cursor-pointer'>
+            {blog.title}
+          </h3>
+
+          <p className='mb-2 text-gray-600'>
+            <FaUser className='inline-flex mr-2 mb-1' />
+            {blog.author}
+          </p>
+
+          <p className='text-sm text-gray-500'>
+            Published: {blog.published_date}
+          </p>
+        </Link>
+      ))}
     </div>
   )
 }
