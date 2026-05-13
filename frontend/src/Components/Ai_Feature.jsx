e34b30f719fa887e214fc218bda2e9b8efec7fc1
Deepak Gupta <154800280+deepak-gupta-git@users.noreply.github.com>
2026-05-13 14:43:15 +0530

    remove (,) Ai_Feature.jsx

--- frontend/src/Components/Ai_Feature.jsx
+++ frontend/src/Components/Ai_Feature.jsx
@@ -24,7 +24,7 @@ const Ai_Feature = () => {
 
     try {
      const response = await axios.post(
-  `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
+  `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${API_KEY}`
           contents: [
             {
               parts: [
