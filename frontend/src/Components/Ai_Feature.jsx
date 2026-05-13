83498b85dd2e3f74c33e60fa382403652422c80d
Deepak Gupta <154800280+deepak-gupta-git@users.noreply.github.com>
2026-05-13 14:42:23 +0530

    Update Using 1.5 flash Version Ai_Feature.jsx

--- frontend/src/Components/Ai_Feature.jsx
+++ frontend/src/Components/Ai_Feature.jsx
@@ -23,9 +23,8 @@ const Ai_Feature = () => {
     setAnswer("");
 
     try {
-      const response = await axios.post(
-        `https://generativelanguage.googleapis.com/v1/models/gemini-2.0-flash:generateContent?key=${API_KEY}`,
-        {
+     const response = await axios.post(
+  `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash:generateContent?key=${API_KEY}`,
           contents: [
             {
               parts: [
